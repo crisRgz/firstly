beec9007c46b3d6b4681791088ffe030fef4abaa
Cristina Rodriguez Cespon <cristina@intern1.local>
2017-10-17 12:51:46 +0100

    Settings changed

--- docroot/wp-config.production.php
+++ docroot/wp-config.production.php
@@ -1,1 +1,1 @@
-require '/srv/www/freistilbox/clients/c11000/s2062/current/config/drupal/settings-d7-db2360.php';
+require_once ('/srv/www/freistilbox/clients/c11000/s2062/current/config/drupal/settings-d7-db2360.php');
